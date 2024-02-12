2f7b1a0d48b946d49c1aaaf79ae081fa17938559
MinZung <90918826+minza55113151@users.noreply.github.com>
2024-02-12 21:12:48 +0700

    change to internal to protect function call from outside to change something in commits

--- CommitReveal.sol
+++ CommitReveal.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract CommitReveal {
 
-  uint8 public max = 100;
+  uint8 internal max = 100;
 
   struct Commit {
     bytes32 commit;
@@ -12,9 +12,9 @@ contract CommitReveal {
     bool revealed;
   }
 
-  mapping (address => Commit) public commits;
+  mapping (address => Commit) internal commits;
 
-  function commit(bytes32 dataHash) public {
+  function commit(bytes32 dataHash) internal {
     commits[msg.sender].commit = dataHash;
     commits[msg.sender].block = uint64(block.number);
     commits[msg.sender].revealed = false;
@@ -22,7 +22,7 @@ contract CommitReveal {
   }
   event CommitHash(address sender, bytes32 dataHash, uint64 block);
 
-  function reveal(bytes32 revealHash) public {
+  function reveal(bytes32 revealHash) internal {
     //make sure it hasn't been revealed yet and set it to revealed
     require(commits[msg.sender].revealed==false,"CommitReveal::reveal: Already revealed");
     commits[msg.sender].revealed=true;
@@ -40,11 +40,11 @@ contract CommitReveal {
   }
   event RevealHash(address sender, bytes32 revealHash, uint random);
 
-  function getHash(bytes32 data) public view returns(bytes32){
+  function getHash(bytes32 data) internal view returns(bytes32){
     return keccak256(abi.encodePacked(address(this), data));
   }
 
-  function revealAnswer(bytes32 answer,bytes32 salt) public {
+  function revealAnswer(bytes32 answer,bytes32 salt) internal {
     //make sure it hasn't been revealed yet and set it to revealed
     require(commits[msg.sender].revealed==false,"CommitReveal::revealAnswer: Already revealed");
     commits[msg.sender].revealed=true;
@@ -54,7 +54,7 @@ contract CommitReveal {
   }
   event RevealAnswer(address sender, bytes32 answer, bytes32 salt);
 
-  function getSaltedHash(bytes32 data,bytes32 salt) public view returns(bytes32){
+  function getSaltedHash(bytes32 data,bytes32 salt) internal view returns(bytes32){
     return keccak256(abi.encodePacked(address(this), data, salt));
   }
 
